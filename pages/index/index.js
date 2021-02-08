6bb9bdc5b3ec14939012fd4e636c5d63ce17f220
Twittytop <hongwei.tu@chukong-inc.com>
2021-02-08 19:00:20 +0800

    chore(*): 加上分页和下拉刷新

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,28 +1,50 @@
 import request from "../../utils/request";
-import { POSTS_URL } from "../../config/index";
+import { POSTS_PAGE_URL } from "../../config/index";
+
+const postInfo = {
+  curPage: 1
+};
 
 Page({
   data: {
-    articleList: []
-  },
-  // 事件处理函数
-  bindArticleTap(e) {
-    var path = e.currentTarget.dataset.path;
-    wx.navigateTo({
-      url: '../article/article?path=' + path
-    });
+    articleList: [],
+    height: 0,
+    isNoMore: false
   },
   onLoad() {
+    // 初始化 高度   
+    wx.getSystemInfo({
+      success: res => {
+        this.setData({
+          height: res.windowHeight
+        });
+        console.log(res)
+      }
+    });
+    this.getPostData(1);
+  },
+  getPostData (page, type) {
     wx.showLoading({
       title: '加载中',
     });
     request
-      .get(POSTS_URL)
+      .get(POSTS_PAGE_URL + page + '.json')
       .then(res => {
         console.log("发起get请求-res", res);
-        this.setData({
-          articleList: res.data
-        });
+        if (page === 1) {
+          postInfo.total = res.pageCount;
+          this.setData({
+            articleList: res.data
+          });
+          if (type === 'refresh') {
+            console.log('refresh')
+          }
+        } else {
+          let articleList = this.data.articleList.concat(res.data);
+          this.setData({
+            articleList
+          });
+        }
       })
       .catch(err => {
         console.log("发起get请求-err", err);
@@ -30,5 +52,25 @@ Page({
       .finally(() => {
         wx.hideLoading();
       });
+  },
+  // 事件处理函数
+  bindArticleTap(e) {
+    var path = e.currentTarget.dataset.path;
+    wx.navigateTo({
+      url: '../article/article?path=' + encodeURIComponent(path)
+    });
+  },
+  lower () {
+    if (postInfo.curPage < postInfo.total) {
+      postInfo.curPage++;
+      this.getPostData(postInfo.curPage);
+    } else {
+      this.setData({
+        isNoMore: true
+      });
+    }
+  },
+  bindRefresherRefresh () {
+    this.getPostData(1, 'refresh');
   }
 })
