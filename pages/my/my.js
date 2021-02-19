45554f2ce95def73d3792080c45ae6b5cfa27931
Twittytop <hongwei.tu@chukong-inc.com>
2021-02-19 17:12:55 +0800

    chore(my): 完善我的页面

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,66 +1,30 @@
-// pages/logs/logs.js
+var scene = wx.getLaunchOptionsSync();
+console.log(scene)
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    scene
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  goMyBlog (e) {
+    wx.setClipboardData({
+      data: e.currentTarget.dataset.clipboardText,
+      success: () => {
+        wx.showToast({
+          title: '博客链接已复制',
+          duration: 1000,
+        });
+      }
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  goMyOfficialAccount (e) {
+    wx.setClipboardData({
+      data: e.currentTarget.dataset.clipboardText,
+      success: () => {
+        wx.showToast({
+          title: '公众号名已复制',
+          duration: 1000,
+        });
+      }
+    });
   }
 })
